36db33f95216b0082dc9bb9f4bddf9912b9a387d
vijybaskar <vijaybaskar.t7@gmail.com>
2022-07-01 21:42:38 +0530

    new changes

---
+++ README.md
@@ -0,0 +1,9 @@
+# VIDEO CHAT APP
+
+React - socketio - simple peer
+
+# BACKEND
+
+check https://github.com/vijaybaskart7/video_chat_server
+
+backend hosted in Heroku - link (`https://vijay-video-chat-server.herokuapp.com/`)
---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://vijay-video-chat-server.herokuapp.com',
+      changeOrigin: true,
+    })
+  );
+};
